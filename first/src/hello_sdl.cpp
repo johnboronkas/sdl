15e72226590fa5456ccc586af391234c15d9ea86
John Boronkas <johnboronkas@gmail.com>
2017-10-10 20:17:52 -0400

    init func

--- first/src/hello_sdl.cpp
+++ first/src/hello_sdl.cpp
@@ -3,29 +3,26 @@
 
 #include <iostream>
 #include <fstream>
-using namespace std;
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+SDL_Window* window = NULL;
+SDL_Surface* screenSurface = NULL;
+
 FILE* log;
 
-void quit(SDL_Window* window) {
+void quit() {
     fclose(log);
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
 
-int main(int argc, char* args[]) {
-    log = freopen ("log.log", "wb", stdout);
-
-    SDL_Window* window = NULL;
-    SDL_Surface* screenSurface = NULL;
-
+bool init() {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("ERROR: SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-        quit(window);
-        return -20;
+        quit();
+        return false;
     }
     
     window = SDL_CreateWindow("SDL Tutorial"
@@ -37,15 +34,24 @@ int main(int argc, char* args[]) {
 
     if (window == NULL) {
         printf("ERROR: Window could not be created! SDL_Error: %s\n", SDL_GetError());
-        quit(window);
-        return -30;
+        quit();
+        return false;
     }
 
     screenSurface = SDL_GetWindowSurface(window);
+    printf("Test2");
+    return true;
+}
+
+int main(int argc, char* args[]) {
+    log = freopen ("log.log", "wb", stdout);
+    init();
+
     SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
     SDL_UpdateWindowSurface(window);
 
     SDL_Delay(2000);
-    quit(window);
+
+    quit();
     return 0;
 }
